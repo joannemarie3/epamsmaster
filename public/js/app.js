9a7322fd0391395cce88bec3a5385492102c9655
Joanne Devera <joannemarie31991@gmail.com>
2016-09-15 13:42:41 +0800

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+<!-- Main js File -->
+
+var main = function(){
+
+  // '.tbl-content' consumed little space for vertical scrollbar, scrollbar width depend on browser/os/platfrom. Here calculate the scollbar width .
+  $(window).on("load resize ", function() {
+    var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+    $('.tbl-header').css({'padding-right':scrollWidth});
+  }).resize();
+
+}
+
+$(document).ready(main);
